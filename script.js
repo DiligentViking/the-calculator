9a21a992dbdbe60fa9b3c150a4da04d16afe0beb
DiligentViking <acore-n@proton.me>
2025-03-25 16:32:02 -0500

    Add basic calc functionality

--- script.js
+++ script.js
@@ -17,16 +17,39 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
   switch (operator) {
     case '+':
-      add(num1, num2);
-      break;
+      return add(num1, num2);
     case '-':
-      subtract(num1, num2);
-      break;
+      return subtract(num1, num2);
     case '*':
-      mulitply(num1, num2);
-      break;
+      return mulitply(num1, num2);
     case '/':
-      divide(num1, num2);
-      break;
+      return divide(num1, num2);
   }
 }
+
+
+let operator;
+let num1;
+let num2;
+
+const calculator = document.querySelector('#calculator');
+const display = document.querySelector('#display');
+calculator.addEventListener('click', (event) => {
+  const button = event.target.id;
+  if ('0123456789'.includes(button)) {
+    display.textContent += button;
+  } else if ('+-*/'.includes(button)) {
+    operator = button;
+    num1 = +display.textContent;
+    display.textContent = '';
+  } else if (button == 'enter') {
+    num2 = +display.textContent;
+    display.textContent = operate(operator, num1, num2);
+  } else if (button == 'clear') {
+    operator = '';
+    num1 = '';
+    num2 = '';
+    display.textContent = '';
+  }
+});
+
